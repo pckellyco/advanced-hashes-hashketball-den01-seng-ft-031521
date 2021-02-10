98bfc25db20ab33c37292ee89c6e4f5635cbd4d4
Paige Kelly <pckelly.co@gmail.com>
2021-02-10 02:41:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -177,9 +177,9 @@ end
 def player_numbers(team)
   team_jersey_nums = []
   game_hash.each do |location, team_info|
+    players_info = game_hash[location][:players]
     if team_info.has_value?(team)
       binding.pry
-      players_info = game_hash[location][:players]
       players_info.each do |hash|
         jersey_number = hash[:number]
         team_jersey_nums.push(jersey_number)
